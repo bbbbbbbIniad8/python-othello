813af86309e8a62eab96bbfd3c71753cb578bbdf
0x7b <bbbbbbbIniad80x7b@users.noreply.github.com>
2025-08-22 13:37:38 +0900

    コードの整形

--- coord.py
+++ coord.py
@@ -1,44 +1,45 @@
 import pygame
 
-def debug_mouse_coord(screen,font):
-        #左上に座標を表示
-        mouseX,mouseY = pygame.mouse.get_pos()
-        text = font.render(f'{mouseX},{mouseY}', True, (255, 255, 255))
-        screen.blit(text, (0, 0))
+#左上に座標を表示
+def debug_mouse_coord(screen, font):
+    mouseX,mouseY = pygame.mouse.get_pos()
+    text = font.render(f'{mouseX},{mouseY}', True, (255, 255, 255))
+    screen.blit(text, (0, 0))
 
-def where_mouse_mass(Mass_size,shift):
-        #マウスのｘｙ座標を分割座標に変換
-        mouseX,mouseY = pygame.mouse.get_pos()
-        N_MassX,N_MassY = ((mouseX - shift[0]) // Mass_size[0] , (mouseY - shift[1]) // Mass_size[1])
-        return N_MassX,N_MassY
+#マウスのｘｙ座標を分割座標に変換
+def where_mouse_mass(Mass_size, shift):
+    mouseX,mouseY = pygame.mouse.get_pos()
+    N_MassX,N_MassY = ((mouseX-shift[0])//Mass_size[0], (mouseY-shift[1])//Mass_size[1])
+    return N_MassX,N_MassY
 
-def exchange_mass(mass_size,X,Y):
-        #指定したｘｙ座標を分割座標に変換
-        N_MassX,N_MassY = (X // mass_size[0] , Y // mass_size[1])
-        return N_MassX,N_MassY
+#指定したｘｙ座標を分割座標に変換
+def exchange_mass(mass_size, X, Y):
+    N_MassX,N_MassY = (X//mass_size[0], Y//mass_size[1])
+    return N_MassX,N_MassY
 
-def exchange_coord(mass_size,shift,X,Y):
-        #分割座標を通常座標に変換
-        N_MassX,N_MassY = (X * mass_size[0] + shift[0] , Y * mass_size[1] + shift[1])
-        return N_MassX,N_MassY
+#分割座標を通常座標に変換
+def exchange_coord(mass_size, shift, X, Y):
+    N_MassX,N_MassY = (X * mass_size[0] + shift[0], Y * mass_size[1] + shift[1])
+    return N_MassX,N_MassY
 
-def tracking_mass(screen,mass_size,shift):
-        #分割座標をもとにマウスカーソルを追いかける形で図形を表示
-        N_MassX,N_MassY = where_mouse_mass(mass_size,shift)
-        pygame.draw.rect(screen,(0,255,255),(N_MassX * mass_size[0] + shift[0] ,N_MassY * mass_size[1] + shift[1], mass_size[0],mass_size[1]),3)
+#分割座標をもとにマウスカーソルを追いかける形で図形を表示
+def tracking_mass(screen,mass_size, shift):
+    N_MassX,N_MassY = where_mouse_mass(mass_size, shift)
+    pygame.draw.rect(screen, (0, 255, 255), (N_MassX*mass_size[0]+shift[0], N_MassY*mass_size[1]+shift[1],
+                                           mass_size[0], mass_size[1]),3)
 
 
 #指定した分割座標上に図形を表示
-def put_on_mass(screen,Mass_size,shift,X,Y,color,KATATI):
-        X = Mass_size[0] * X + shift[0]
-        Y = Mass_size[1] * Y + shift[1]
-        if KATATI == "丸":
-            pygame.draw.circle(screen, color, (X + Mass_size[0]//2, Y + Mass_size[0]//2), Mass_size[0] // 2 - 0.5)
+def put_on_mass(screen, Mass_size, shift, X, Y, color, KATATI):
+    X = Mass_size[0] * X + shift[0]
+    Y = Mass_size[1] * Y + shift[1]
+    if KATATI == "丸":
+        pygame.draw.circle(screen, color, (X+Mass_size[0]//2, Y+Mass_size[0]//2), Mass_size[0]//2-0.5)
             
-        elif KATATI =="四角":
-            pygame.draw.rect(screen, color, (X, Y, Mass_size[0], Mass_size[1]), 1)
+    elif KATATI =="四角":
+        pygame.draw.rect(screen, color, (X, Y, Mass_size[0], Mass_size[1]), 1)
 
-        elif KATATI == "小四角":
-            MASSX = Mass_size[0]//4
-            MASSY = Mass_size[1]//4
-            pygame.draw.rect(screen, color, (X+ Mass_size[0]//2 - MASSX/2,Y+ Mass_size[1]//2 - MASSY/2, MASSX, MASSY))
+    elif KATATI == "小四角":
+        MASSX = Mass_size[0] // 4
+        MASSY = Mass_size[1] // 4
+        pygame.draw.rect(screen, color, (X+Mass_size[0]//2-MASSX/2,Y+Mass_size[1]//2-MASSY/2, MASSX, MASSY))
--- core.py
+++ core.py
@@ -3,16 +3,16 @@ import time
 from coord import tracking_mass,put_on_mass,where_mouse_mass
 
 
-class Osero:
+class othello:
     def __init__(self):
         #setting
         pygame.init()
-        self.mass_size = [60,60]
-        self.shift = [120,30]
+        self.mass_size = [60, 60]
+        self.shift = [120, 30]
         windowX, windowY = 850, 650
         self.font = pygame.font.Font('ipaexg.ttf', 20)
         self.screen = pygame.display.set_mode((windowX, windowY))
-        self.RGBdict = {"WHITE":(240,240,240),"BLACK":(50,50,50),"GREAN":(118,190,180)}
+        self.RGBdict = {"WHITE": (240, 240, 240),"BLACK": (50, 50, 50),"GREAN": (118, 190, 180)}
         self.evaluation_lst = [
             [50 ,-20, 15, 10, 10, 15, -20, 50],
             [-20,-40, -5, -5, -5, -5, -40,-20],
@@ -23,6 +23,7 @@ class Osero:
             [-20,-40, -5, -5, -5, -5, -40,-20],
             [50 ,-20, 15, 10, 10, 15, -20, 50],
         ]
+
 
     def create_board_lst(self):
         board = [
@@ -36,7 +37,8 @@ class Osero:
             [-1,-1,-1,-1,-1,-1,-1,-1],
         ]
         return board
-    
+
+
     def set_board(self,board_lst):
         board_lst[3][3] = 1
         board_lst[3][4] = 0
@@ -44,37 +46,39 @@ class Osero:
         board_lst[4][4] = 1
         return board_lst
 
+
     def draw_board_line(self):
         for YC in range(8):
             for XC in range(8):
-                put_on_mass(self.screen,self.mass_size,self.shift,XC,YC,(0,0,0),"四角")
+                put_on_mass(self.screen, self.mass_size, self.shift, XC, YC, (0, 0, 0), "四角")
+
 
     def draw_now_board(self,board_lst,search_lst):
         for YC in range(8):
             for XC in range(8):
                 if board_lst[YC][XC] == 0:
-                    put_on_mass(self.screen,self.mass_size,self.shift,XC,YC,self.RGBdict["BLACK"],"丸")
+                    put_on_mass(self.screen, self.mass_size, self.shift, XC, YC, self.RGBdict["BLACK"], "丸")
                 elif board_lst[YC][XC] == 1:
-                    put_on_mass(self.screen,self.mass_size,self.shift,XC,YC,self.RGBdict["WHITE"],"丸")
+                    put_on_mass(self.screen, self.mass_size, self.shift, XC, YC, self.RGBdict["WHITE"], "丸")
                 elif board_lst[YC][XC] == -1 and search_lst[YC][XC] > 0:
-                    put_on_mass(self.screen,self.mass_size,self.shift,XC,YC,(100,100,0),"小四角")
+                    put_on_mass(self.screen, self.mass_size, self.shift, XC, YC, (100, 100, 0), "小四角")
 
-    def put_stone(self,board_lst,color,X,Y):
+
+    def put_stone(self, board_lst, color, X, Y):
         putcolor = self.RGBdict["BLACK"] if color == 0 else self.RGBdict["WHITE"]
-        if X < 8:
-            if Y < 8:
-                if board_lst[Y][X] == -1:
-                    board_lst[Y][X] = color
-                    put_on_mass(self.screen,self.mass_size,self.shift,X,Y,putcolor,"丸")
-                    time.sleep(0.1)
-                    pass_ = 0
+        if board_lst[Y][X] == -1:
+            board_lst[Y][X] = color
+            put_on_mass(self.screen, self.mass_size, self.shift, X, Y, putcolor, "丸")
+            time.sleep(0.1)
+            pass_ = 0
         return board_lst
 
-    def check_change_stone(self,board_lst,color,mode,X,Y):
+
+    def check_change_stone(self, board_lst, color, mode, X, Y):
         opposite_color = self.opposite(color)
         inversion_num = 0
-        ng_lst = [-1,8]
-        pies = [[1,0],[1,1],[0,1],[-1,1],[-1,0],[-1,-1],[0,-1],[1,-1]]
+        ng_lst = [-1, 8]
+        pies = [[1,0], [1,1], [0,1], [-1,1], [-1,0], [-1,-1], [0,-1], [1,-1]]
         for p in pies:
             check_X,check_Y = X + p[0], Y + p[1]
             if check_X not in ng_lst and check_Y not in ng_lst:
@@ -91,14 +95,14 @@ class Osero:
                                 inversion_num += 1
                             break
         return board_lst if mode == "exe" else inversion_num
-    
+
 
     def search_can_put_lst(self,board_lst,color):
         result_lst = self.create_board_lst()
         for X in range(8):
             for Y in range(8):
                 if board_lst[Y][X] == -1:
-                    cnt = self.check_change_stone(board_lst,color,"cnt",X,Y)
+                    cnt = self.check_change_stone(board_lst, color, "cnt", X, Y)
                     result_lst[Y][X] = cnt
         return result_lst
 
@@ -110,21 +114,24 @@ class Osero:
             target_X,target_Y = target_X + direction[0], target_Y + direction[1]
         return board_lst
 
-    def opposite(self,color):
+
+    def opposite(self, color):
         return 0 if color == 1 else 1
-    
-    def AI_put_where(self,board_lst,evaluation_lst,color):
-        search_lst = self.search_can_put_lst(board_lst,color)
-        decide_put = [(-1,-1),-100]
+
+
+    def AI_put_where(self, board_lst, evaluation_lst, color):
+        search_lst = self.search_can_put_lst(board_lst, color)
+        decide_put = [(-1,-1), -100]
         for Y in range(8):
             for X in range(8):
-                if board_lst[Y][X] == -1 and search_lst[Y][X] > 0 and self.check_change_stone(board_lst,color ,"cnt",X,Y) > 0:
+                if board_lst[Y][X] == -1 and search_lst[Y][X] > 0 and self.check_change_stone(board_lst,color ,"cnt", X, Y) > 0:
                     if decide_put[1] < evaluation_lst[Y][X]:
-                        decide_put[0] = (X,Y)
+                        decide_put[0] = (X, Y)
                         decide_put[1] = evaluation_lst[Y][X]
 
         return decide_put[0]
-    
+
+
     def coune_stone(self,board_lst):
         black = 0
         white = 0
@@ -150,7 +157,6 @@ class Osero:
         screen.blit(text1, (X+40, Y+10))
         screen.blit(text2, (X+40, Y+60))
 
-        
 
     def run(self):
         board_lst = self.set_board(self.create_board_lst())
@@ -158,7 +164,6 @@ class Osero:
         player_color = 0
         enemy_color = 1
         now_put_color = 0
-
         while True:
             for event in pygame.event.get():
                 if event.type == pygame.QUIT:
@@ -175,12 +180,12 @@ class Osero:
                 X,Y = where_mouse_mass(self.mass_size,self.shift)
                 if event.type == pygame.MOUSEBUTTONDOWN and X >= 0 and X < 8 and Y >= 0 and Y < 8 and board_lst[Y][X] == -1:
                     if self.check_change_stone(board_lst,now_put_color ,"cnt",X,Y) >= 1:
-                        board_lst = self.put_stone(board_lst,now_put_color,X,Y)
-                        board_lst = self.check_change_stone(board_lst,now_put_color ,"exe",X,Y)
+                        board_lst = self.put_stone(board_lst, now_put_color, X, Y)
+                        board_lst = self.check_change_stone(board_lst, now_put_color, "exe", X, Y)
                         turn += 1
 
                 black,white,none = self.coune_stone(board_lst)
-                self.put_judge_num(self.screen,white,black,self.font,600,30)
+                self.put_judge_num(self.screen, white, black, self.font, 600, 30)
                         
                 pygame.display.flip()
             else:
@@ -191,9 +196,7 @@ class Osero:
                 board_lst = self.check_change_stone(board_lst,now_put_color ,"exe",X,Y)
                 turn += 1
                 pygame.display.flip()
-            
-            
-            
 
-main = Osero()
+
+main = othello()
 main.run()
